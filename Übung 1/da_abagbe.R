65861d145582aaee1788d5d542368e1208ace119
Maximilian Hagn <maximilian.hagn@gmail.com>
2022-04-20 23:34:22 +0200

    first commit

---
+++ Übung 1/da_abagbe.R
@@ -0,0 +1,10 @@
+---
+title: "Datenanalyse"
+subtitle: "Abgabe 2"
+author: "Maximilian Hagn (Matr. Nr. 11808237)"
+date: "20.05.2020"
+output: pdf_document
+---
+
+## Erstes Beispiel
+Erstellen
---
+++ Übung 2/da_abagbe.R
@@ -0,0 +1,10 @@
+---
+title: "Datenanalyse"
+subtitle: "Abgabe 2"
+author: "Maximilian Hagn (Matr. Nr. 11808237)"
+date: "21.05.2020"
+output: pdf_document
+---
+
+## Erstes Beispiel
+Erstelleninstall.package("Quandl")
